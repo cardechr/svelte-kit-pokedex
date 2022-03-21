452f6720c317bec6a1bb2da3af7455b0078a5b6e
Chris Cardenas <cardechr.dev@gmail.com>
2022-03-21 15:25:14 -0500

    deploy to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import WindiCSS from 'vite-plugin-windicss';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [preprocess()],
 	kit: {
-		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/svelte-kit-pokedex'
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		  }),
 		vite: {
 			plugins: [
 				WindiCSS(),
